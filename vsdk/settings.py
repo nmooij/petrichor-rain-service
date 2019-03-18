932a1171408ed519e023bd97d217b7248d699df2
Natasja <n.mooij@student.vu.nl>
2019-03-18 21:33:35 +0100

    'msg'

--- vsdk/settings.py
+++ vsdk/settings.py
@@ -198,13 +198,30 @@ if HEROKU:
             'PORT':'',
         }
     }
-    import dj_database_url
+    #import dj_database_url
     #db_from_env = dj_database_url.config(conn_max_age=500)
     #DATABASES['default'].update(db_from_env)
-    DATABASES = {'default': dj_database_url.config()}
+    
 else:
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+###################
+in_heroku = False
+if 'DATABASE_URL' in os.environ:
+    in_heroku = True
+
+import dj_database_url
+if in_heroku:
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+#######################
 
 #Only set this to True when sox and mediainfo are available, and local storage is used for static files.
 KASADAKA = False
