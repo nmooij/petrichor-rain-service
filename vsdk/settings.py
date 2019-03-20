8118cd537adcf3e070b32edb01afb487e55847dc
Natasja <n.mooij@student.vu.nl>
2019-03-20 19:44:45 +0100

    update

--- vsdk/settings.py
+++ vsdk/settings.py
@@ -201,6 +201,7 @@ if HEROKU:
     import dj_database_url
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
+   
     
 else:
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
