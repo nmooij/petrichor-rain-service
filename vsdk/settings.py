feb4d72283119836d1d1e0e833b4a458bac28c4b
Natasja <n.mooij@student.vu.nl>
2019-03-22 17:29:13 +0100

    Update

--- vsdk/settings.py
+++ vsdk/settings.py
@@ -97,11 +97,23 @@ WSGI_APPLICATION = 'vsdk.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+#DATABASES = {
+ #    'default': {
+#		 'ENGINE': 'django.db.backends.sqlite3',
+#		 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#	     }}
+
+
 DATABASES = {
-     'default': {
-		 'ENGINE': 'django.db.backends.sqlite3',
-		 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-	     }}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DB',
+        'USER': 'Admin',
+        'PASSWORD': 'SuperUser',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
