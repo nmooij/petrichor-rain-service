aae7f5295e54f89047c9b4fe3660e37c885f5427
Natasja <n.mooij@student.vu.nl>
2019-04-09 15:10:10 +0200

    Update

--- vsdk/service_development/views/vse_input.py
+++ vsdk/service_development/views/vse_input.py
@@ -27,7 +27,7 @@ def input_generate_context(input_element, session):
 
 
 def Input(request, element_id, session_id):
-    input_element = get_object_or_404(Input, pk=element_id)
+    input_element = get_object_or_404(InputData, pk=element_id)
     voice_service = input_element.service
     session = lookup_or_create_session(voice_service, session_id)
 
