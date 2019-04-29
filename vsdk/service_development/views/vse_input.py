6166551f38bdf4e7a67454c50e26949e0f9207ca
Natasja <n.mooij@student.vu.nl>
2019-04-29 19:44:18 +0200

    Update

--- vsdk/service_development/views/vse_input.py
+++ vsdk/service_development/views/vse_input.py
@@ -45,8 +45,7 @@ def InputData(request, element_id, session_id):
 
         result = UserInput()
 
-        #result.input_value = request.POST.get("input_value")
-        result.input_value = request.POST['input_value']
+        result.input_value = request.POST.get('input_value')
         result.session = session
         result.category = input_element.input_category 
 
