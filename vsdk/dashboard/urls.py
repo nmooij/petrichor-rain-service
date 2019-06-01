6cd82fe97b6b97331f24ca6d51dd871471b43c9e
Natasja <n.mooij@student.vu.nl>
2019-06-01 21:24:39 +0200

    Update

--- vsdk/dashboard/urls.py
+++ vsdk/dashboard/urls.py
@@ -6,10 +6,7 @@ from . import views
 
 from django.conf.urls import url, include
 
-# urlpatterns = [
-    # path('', views.index, name='index'),
-# ]
 
 urlpatterns = [
-    url(r'^$', views.myFirstChart, name = 'demo'),
+    url(r'^$', views.Chart, name = 'Chart'),	
 ]
